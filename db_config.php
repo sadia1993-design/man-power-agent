6772fe026c44d6eaa3e7a474f4a421210e0be232
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-01-28 17:31:00 +0600

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "man_power");
+$conn = new mysqli("b01hqwwlh4xdsxogk5fw-mysql.services.clever-cloud.com", "u3uf4pgtge5kd9y4", "tDCjw71Jptq0j8K5JGCQ", "b01hqwwlh4xdsxogk5fw");
 
 if (!$conn) {
     die(mysqli_error($conn));
